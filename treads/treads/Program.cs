5491227be35a15d18f9fa63ce6056ab16146e441
Warriorroq <Warriorroq@gmail.com>
2020-12-18 21:55:55 +0200

    F@k - Failuer @ kite

--- treads/treads/Program.cs
+++ treads/treads/Program.cs
@@ -11,29 +11,24 @@ namespace treads
 {
     class Program
     {
+        private static string path = "../../output.xml";
         static void Main(string[] args)
         {
             List<Team> group = new List<Team>();
             
-            if(false)
+            if(true)
             {
-                XmlDocument xDoc = new XmlDocument();
-                xDoc.Load("../../output.xml");
-                XmlElement xRoot = xDoc.DocumentElement;
-                foreach (XmlElement xnode in xRoot)
-                {
-                    Team t = new Team();
-                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
-                    if (attr != null)
-                        t.name = attr.Value;
-
-                    foreach (XmlNode childnode in xnode.ChildNodes)
-                    {
-                        //if (childnode.Name == "")
-                    }
-                    group.Add(t);
-                }                
+                XmlSerializer xmlSerialaizer = new XmlSerializer(typeof(Team));
+                Team bludaToRead = new Team();
+                FileStream fr = new FileStream(path, FileMode.Open);
+                bludaToRead = (Team)xmlSerialaizer.Deserialize(fr);
+                group.Add(bludaToRead);
+                fr.Close();
             }
+
+            foreach (Team t in group)
+                t.WriteDownInfo();
+
             while (true)
             {
                 Console.WriteLine("1 - create random : 2 - create yours : 3 - save table :-num - show team info : 4 - end ");
@@ -55,9 +50,9 @@ namespace treads
                 else if (command == 3)
                 {
                     XmlSerializer xmlSerialaizer = new XmlSerializer(typeof(Team));
-                    FileStream fw = new FileStream("../../output.xml", FileMode.Create);
-                    foreach (Team t in group)
-                        xmlSerialaizer.Serialize(fw, t);
+                    FileStream fw = new FileStream(path, FileMode.Create);
+                    //foreach (Team t in group)
+                    xmlSerialaizer.Serialize(fw, group[0]);
                     fw.Close();
                 }
                 else if (command == 4)
@@ -65,7 +60,7 @@ namespace treads
                 else if (command <= 0)
                 {
                     command *= -1;
-                    if (group.Count > command && command != 0)
+                    if (group.Count >= command && command != 0)
                         group[command - 1].WriteDownInfo();
                 }
             }
